2b64df4d369fda6f00ce961b0036551ff88a09bd
Kevin <rkevinr@kevins-macbook-pro.local>
2016-07-03 14:11:17 -0500

    Minor progress on activity-entry UI design/layout

--- TDT/_controller/TDTEditActivityViewCtlr.swift
+++ TDT/_controller/TDTEditActivityViewCtlr.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class TDTEditActivityViewCtlr: UIViewController {
+class TDTEditActivityViewCtlr: UIViewController
+                    /* , UITextFieldDelegate */ {
+
+    
+    // @IBOutlet weak var activityTextFld: UITextField!    // FIXME: curr'ly extraneous
+    @IBOutlet weak var durationMinsTextFld: UITextField!
+    @IBOutlet weak var durEditingToolbar: UIToolbar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        durationMinsTextFld.inputAccessoryView = durEditingToolbar
+        // durationMinsTextFld.sizeToFit()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,5 +38,16 @@ class TDTEditActivityViewCtlr: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    // MARK: activity text field methods
+    @IBAction func textEntryDone(textField: UITextField) {
+        print("input:  \(textField.text!)")
+    }
+    
+    /*  NOT USED
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        return true
+    }
+    */
+    
 }
